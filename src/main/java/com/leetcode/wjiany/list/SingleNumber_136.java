9e5de79446f4c08b9afab8a5c559fa7e71136aea
wjiany <open_to@163.com>
2019-09-05 23:24:15 +0800

    136只出现一次数字 按位异或

---
+++ src/main/java/com/leetcode/wjiany/list/SingleNumber_136.java
@@ -0,0 +1,36 @@
+package com.leetcode.wjiany.list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 只出现一次的数字
+ */
+public class SingleNumber_136 {
+
+
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for (int i = 0;i<nums.length;i++){
+            a^=nums[i];
+        }
+        return a;
+    }
+    /**
+     * 时间On
+     * 空间On
+     * @param nums
+     * @return
+     */
+    public int singleNumber_2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                set.remove(nums[i]);
+            }else {
+                set.add(nums[i]);
+            }
+        }
+        return set.iterator().next();
+    }
+}
