1f95b2fa39b51c6ce97ffd86f93f20b5d3731036
wjiany <open_to@163.com>
2019-09-07 00:52:19 +0800

    15 三个数字相加

--- src/main/java/com/leetcode/wjiany/Array2P/ThreeSum_15.java
+++ src/main/java/com/leetcode/wjiany/Array2P/ThreeSum_15.java
@@ -16,42 +16,30 @@ public class ThreeSum_15 {
         System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
     }
     public List<List<Integer>> threeSum(int[] nums) {
-        int length = nums.length;
-        Arrays.sort(nums);
-        Map<Integer,Integer> map = new HashMap();
-        List<List<Integer>> lists = new ArrayList<>();
-        for (int i=0 ; i<length ; i++){
-            if(nums[i]>0){
-                continue;
-            }
-            for (int j=i+1 ; j<length; j++){
-                if(nums[i]+nums[j]>0){
-                    continue;
-                }
-                if(map.containsKey(-nums[i]-nums[j])){
-                    lists.add(Arrays.asList(nums[i],-nums[i]-nums[j],nums[j]));
-                    if(map.get(-nums[i]-nums[j]) == 1){
-                        map.remove(-nums[i]-nums[j]);
-                    }else {
-                        map.put(-nums[i]-nums[j],map.get(-nums[i]-nums[j])-1);
+            List<List<Integer>> ans = new ArrayList();
+            int len = nums.length;
+            if(nums == null || len < 3) return ans;
+            Arrays.sort(nums); // 排序
+            for (int i = 0; i < len ; i++) {
+                if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+                if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+                int L = i+1;
+                int R = len-1;
+                while(L < R){
+                    int sum = nums[i] + nums[L] + nums[R];
+                    if(sum == 0){
+                        ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                        while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                        while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                        L++;
+                        R--;
                     }
-                    if( i == j-2){
-                        i = j;
-                    }
-                    break;
-                }
-                if(map.containsKey(nums[j])){
-                    map.put(nums[j],map.get(nums[j])+1);
-                    if(i == j-1){
-                        i = j;
-                    }
-                }else {
-                    map.put(nums[j],1);
+                    else if (sum < 0) L++;
+                    else if (sum > 0) R--;
                 }
             }
+            return ans;
         }
-        return lists;
-    }
 
     public List<List<Integer>> threeSum_error(int[] nums) {
         int length = nums.length;
