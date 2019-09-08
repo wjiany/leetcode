e7b7431b81cfc746cf8e4d21476d793c576aa42a
wjiany <open_to@163.com>
2019-09-08 10:27:13 +0800

    107-637二叉树层次遍历从下到上和平均值

---
+++ src/main/java/com/leetcode/wjiany/tree/LevelOrderBottom_107.java
@@ -0,0 +1,30 @@
+package com.leetcode.wjiany.tree;
+
+import java.util.*;
+
+public class LevelOrderBottom_107 {
+    List<List<Integer>> lists = new LinkedList<>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if(root == null){
+            return lists;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0;i<size;i++){
+                TreeNode remove = queue.remove();
+                list.add(remove.val);
+                if(remove.left!=null){
+                    queue.add(remove.left);
+                }
+                if(remove.right!=null){
+                    queue.add(remove.right);
+                }
+            }
+            lists.add(0,list);
+        }
+        return lists;
+    }
+}
